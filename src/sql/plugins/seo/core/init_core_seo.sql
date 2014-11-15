36a8e65ad27cc0db6148606b23d1fe9f5a743671
pierrelevy <pierrelevy@users.noreply.github.com>
2014-11-15 22:25:55 +0000

    SEO-8 : SQL init script : Keyword REPLACE not supported by PostgeSQL
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/seo/plugin-seo@72235 bab10101-e421-0410-a517-8ce0973de3ef

--- src/sql/plugins/seo/core/init_core_seo.sql
+++ src/sql/plugins/seo/core/init_core_seo.sql
@@ -1,7 +1,6 @@
 --
 -- Dumping data for table core_admin_right
 --
-DELETE FROM core_admin_right WHERE id_right = 'SEO_MANAGEMENT';
 INSERT INTO core_admin_right (id_right,name,level_right,admin_url,description,is_updatable,plugin_name,id_feature_group,icon_url,documentation_url, id_order ) VALUES 
 ('SEO_MANAGEMENT','seo.adminFeature.seo_management.name',1,'jsp/admin/plugins/seo/ManageSEO.jsp','seo.adminFeature.seo_management.name',0,'seo',NULL,NULL,NULL,4);
 
@@ -9,15 +8,14 @@ INSERT INTO core_admin_right (id_right,name,level_right,admin_url,description,is
 --
 -- Dumping data for table core_user_right
 --
-DELETE FROM core_user_right WHERE id_right = 'SEO_MANAGEMENT';
 INSERT INTO core_user_right (id_right,id_user) VALUES ('SEO_MANAGEMENT',1);
 
 
 --
 -- Dumping data for table core_datastore
 --
-REPLACE INTO `core_datastore` VALUES 
-('seo.rewrite.config.lastUpdate','Dernière mise à jour du fichier de configuration : 3 oct. 2012 23:31:51 Nombre d\'url : 11 Resultat : OK'),
+INSERT INTO core_datastore VALUES 
+('seo.rewrite.config.lastUpdate',''),
 ('seo.config.uptodate','false'),
 ('seo.generator.option.addPath','false'),
 ('seo.generator.option.addHtmlSuffix','true'),
@@ -25,5 +23,4 @@ REPLACE INTO `core_datastore` VALUES
 ('seo.generator.daemon.enabled','true'),
 ('seo.canonicalUrls.enabled','true'),
 ('seo.sitmap.daemon.enabled','true'),
-('seo.sitemap.update.log','Dernière génération : 4 oct. 2012 00:29:02 Nombre d\'url : 8 Resultat : OK');
-
+('seo.sitemap.update.log','');
